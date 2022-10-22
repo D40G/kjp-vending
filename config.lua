a4725075132b09cfce8cdcbc3a707c8ba8bc5a7c
Jake <107344765+KingJacobPlayz@users.noreply.github.com>
2022-10-22 03:33:38 -0400

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,61 @@
+Config = {}
+
+Config.VendingItem = {
+    [1] = {
+        name = "kurkakola",
+        price = 18,
+        amount = 50,
+        info = {},
+        type = "item",
+        slot = 1,
+    },
+}
+
+Config.CoffeeItem = {
+    [1] = {
+        name = "coffee",
+        price = 15,
+        amount = 50,
+        info = {},
+        type = "item",
+        slot = 1,
+    },
+}
+
+Config.WaterItem = {
+    [1] = {
+        name = "water_bottle",
+        price = 15,
+        amount = 50,
+        info = {},
+        type = "item",
+        slot = 1,
+    },
+}
+
+Config.SnackItem = {
+    [1] = {
+        name = "twerks_candy",
+        price = 8,
+        amount = 50,
+        info = {},
+        type = "item",
+        slot = 1,
+    },
+    [2] = {
+        name = "snikkel_candy",
+        price = 8,
+        amount = 50,
+        info = {},
+        type = "item",
+        slot = 2,
+    },
+    [3] = {
+        name = "sandwich",
+        price = 15,
+        amount = 50,
+        info = {},
+        type = "item",
+        slot = 3,
+    },
+}
